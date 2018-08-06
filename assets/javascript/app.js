5134680bad4199a57c3f92691f36b40abc46bfd0
jedi225 <jedtk@uci.edu>
2018-08-06 01:58:49 -0700

    JS now appends firebase data to DOM

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,6 +13,35 @@ $(document).ready(function () {
 
     var database = firebase.database();
 
-    
+    $("#submitBTN").click(function (event) {
+        //event.preventDefault();
+
+        var trainName = $("#train-name").val().trim();
+        var destinationName = $("#destination-name").val().trim();
+        var trainTime = $("#train-time").val().trim();
+        var trainFrequency = $("#train-frequency").val().trim();
+
+        database.ref().push({
+            name: trainName,
+            destination: destinationName,
+            time: trainTime,
+            frequency: trainFrequency
+        });
+
+
+
+    });
+
+    database.ref().on("child_added", function (snapshot) {
+        var userTrain = $("<td>").text(snapshot.val().name);
+        var userDestination = $("<td>").text(snapshot.val().destination);
+        var userTime = $("<td>").text(snapshot.val().time);
+        var userFrequency = $("<td>").text(snapshot.val().frequency);
+
+        var $tbody = $("tbody");
+        var $tr = $("<tr>");
+        $tr.append(userTrain, userDestination, userFrequency, /*MoreStuff*/);
+        $tbody.append($tr)
+    });
 
 });
