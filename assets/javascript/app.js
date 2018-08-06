b91722b4fdcf3284bfe8a38aeee76b4c8598d276
jedi225 <jedtk@uci.edu>
2018-08-05 23:05:21 -0700

    Added firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyATT8P12jN5iV-GSIBo35lHCp6nQJEJ3vw",
+        authDomain: "train-scheduler-e689a.firebaseapp.com",
+        databaseURL: "https://train-scheduler-e689a.firebaseio.com",
+        projectId: "train-scheduler-e689a",
+        storageBucket: "train-scheduler-e689a.appspot.com",
+        messagingSenderId: "347399618930"
+    };
+    firebase.initializeApp(config);
+
+
+
+});
