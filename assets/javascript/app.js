6a568470c3ce40c343fceca390e721e4a01181bf
jedi225 <jedtk@uci.edu>
2018-08-07 02:20:35 -0700

    Fully working app

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
     var database = firebase.database();
 
     $("#submitBTN").click(function (event) {
+        //I want the default to occur on the submit to clear out the text boxes
         //event.preventDefault();
 
         var trainName = $("#train-name").val().trim();
@@ -32,16 +33,62 @@ $(document).ready(function () {
 
     });
 
+    //Displays firebase data to DOM
     database.ref().on("child_added", function (snapshot) {
         var userTrain = $("<td>").text(snapshot.val().name);
         var userDestination = $("<td>").text(snapshot.val().destination);
         var userTime = $("<td>").text(snapshot.val().time);
         var userFrequency = $("<td>").text(snapshot.val().frequency);
 
+        var tFrequency = snapshot.val().frequency;
+        var firstTime = snapshot.val().time;
+        console.log("First Time: " + firstTime);
+
+        //current time
+        var currentTime = moment();
+        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+        // First Time (pushed back 1 year to make sure it comes before current time)
+        var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+        console.log("Converted First Time: " + firstTimeConverted);
+
+        // Difference between the times
+        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        console.log("DIFFERENCE IN TIME: " + diffTime);
+        console.log("frequency: " + tFrequency);
+
+        // Time apart (remainder)
+        var tRemainder = diffTime % tFrequency;
+        console.log(tRemainder);
+
+        // Minute Until Train
+        var tMinutesTillTrain = tFrequency - tRemainder;
+        console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+        // Next Train
+        var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+        console.log("ARRIVAL TIME: " + moment(nextTrain).format('LT'));
+
+
+        var nextArrival = $("<td>").text(moment(nextTrain).format('LT'));
+        var minutesAway = $("<td>").text(tMinutesTillTrain);
+
+        //Appends to DOM
         var $tbody = $("tbody");
         var $tr = $("<tr>");
-        $tr.append(userTrain, userDestination, userFrequency, /*MoreStuff*/);
+        $tr.append(userTrain, userDestination, userFrequency, nextArrival, minutesAway);
         $tbody.append($tr)
     });
+
+    //Refreshes every 5 minutes
+    setInterval(function () {
+        location.reload();
+    }, 300000);
+
+    //Refresh button to manually refresh
+    $("#refresh-button").click(function () {
+        location.reload();
+
+    })
 
 });
